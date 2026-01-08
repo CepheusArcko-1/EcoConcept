648eb38dc4d7d1bd803fa8d684df7d261b2022c1
Beturan <119627642+Beturan@users.noreply.github.com>
2026-01-08 12:33:33 +0100

    Update index.php

--- site/index.php
+++ site/index.php
@@ -67,7 +67,7 @@ session_start();
 <footer id="footer">
 <ul class="footer-links">
     <li class="footer-item">©Projet 3iL</li>
-    <li class="footer-item"><a href="#" target="_blank">><img id="logo" src="images/facebook.webp" alt="Lien vers Facebook"></a></li>
+    <li class="footer-item"><a href="#" target="_blank"><img id="logo" src="images/facebook.webp" alt="Lien vers Facebook"></a></li>
     <li class="footer-item">Site test</li>
 </ul>
 </footer>
